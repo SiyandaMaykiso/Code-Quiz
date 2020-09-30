77db1fe71f029c4b54f74f71fbe64f7dc1aa06a9
Iamsiyanda <s.mayekiso@gmail.com>
2020-09-30 10:52:32 +0200

    Updates to document

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// Below are the multiple choice questions and their answers
+
+var questions = [{
+    title: "What does HTML stands for?",
+    choices: ["Hypertext Machine language.( )", "Hypertext and links markup language.( )", "Hypertext Markup Language( )", "Hightext machine language.( )"],
+    answer: "Hypertext Markup Language.( )"
+},
+{
+    title: "How is document type initialized in HTML5.?",
+    choices: ["</DOCTYPE HTML>( )", "</DOCTYPE>( )", "<!DOCTYPE HTML>( )", "</DOCTYPE html>( )"],
+    answer: "<!DOCTYPE HTML> ( )"
+},
+{
+    title: " Which of the following HTML Elements is used for making any text bold?",
+    choices: ["<p>( )", "<i>( )", "<li>( )", "<b>"],
+    answer: "<b>( )"
+},
+{
+    title: "Which of the following HTML element is used for creating an unordered list?",
+    choices: ["<ui>( )", "<i>( )", "<em>( )", "<ul>( )"],
+    answer: "<ul>( )"
+},
+{
+    title: "Which of the following attributes is used to open an hyperlink in new tab?",
+    choices: ["tab( )", "href( )", "target( )", "ref( )"],
+    answer: "target( )"
+
+},
+{
+    title: "What is the purpose of using div tags in HTML?",
+    choices: ["For creating Different styles.( )", "For creating different sections.( )", "For adding headings. ( )", "For adding titles.( )"],
+    answer: "For creating different sections.( )"
+}
+]
