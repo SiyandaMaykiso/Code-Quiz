a38e87bc90d14eaeebb9da52d66d3869db845cc6
Iamsiyanda <s.mayekiso@gmail.com>
2020-10-02 08:30:18 +0200

    Updates to index

--- script.js
+++ script.js
@@ -187,5 +187,21 @@
     previousButton.addEventListener("click", showPreviousSlide);
     nextButton.addEventListener("click", showNextSlide);
     
+    // Below is the timer
 
+    window.onload = function() {
+        var minute = 5;
+        var sec = 60;
+        setInterval(function() {
+          document.getElementById("timer").innerHTML = minute + " : " + sec;
+          sec--;
+          if (sec == 00) {
+            minute --;
+            sec = 60;
+            if (minute == 0) {
+              minute = 5;
+            }
+          }
+        }, 1000);
+      }
 })();
